5f41d80025ccdd50fa6313868b5e292a9ac91603
samil <studi.msa@gmail.com>
2023-07-11 12:51:24 +0300

    Initial Commit

---
+++ Telegram Colne/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Telegram Colne
+//
+//  Created by muhammed samil demir on 11.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Telegram Colne/Telegram_ColneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Telegram_ColneApp.swift
+//  Telegram Colne
+//
+//  Created by muhammed samil demir on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Telegram_ColneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
